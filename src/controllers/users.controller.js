1b67de4da6bb36dc002c924f66c7362630741dfc
JosePerez <joseleopepe1033@gmail.com>
2024-08-05 00:58:03 -0500

    Cambio de nombres de tablas Users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,7 +2,7 @@ import {pool} from '../db.js'
 
 export const getUsers = async(req,res) => {
     try {
-        const [users] = await pool.query('SELECT * from users')
+        const [users] = await pool.query('SELECT * from Users')
         res.json(users)
     } catch (error) {
         return res.status(500).json({
@@ -14,7 +14,7 @@ export const getUsers = async(req,res) => {
 export const getUser = async(req, res) => {
     const {id} = req.params
     try {
-        const [users] = await pool.query("SELECT * FROM users WHERE id_user = ?", [id])
+        const [users] = await pool.query("SELECT * FROM Users WHERE id_user = ?", [id])
         if (users.length <= 0) return res.status(404).json({
             message : "No se ha encontrado ningún usuario"
         })
@@ -29,7 +29,7 @@ export const getUser = async(req, res) => {
 export const postUsers = async(req,res) => {
     const {names, lastnames, dni, mail, password, phone_number,} = req.body
     try {
-        const [user] = await pool.query('INSERT INTO users (names, lastnames, dni, mail, password, phone_number) VALUES (?, ?, ?, ?, ?, ?)', [names, lastnames, dni, mail, password, phone_number])
+        const [user] = await pool.query('INSERT INTO Users (names, lastnames, dni, mail, password, phone_number) VALUES (?, ?, ?, ?, ?, ?)', [names, lastnames, dni, mail, password, phone_number])
         res.send({
             id: user.insertId,
             names,
@@ -50,11 +50,11 @@ export const putUsers = async(req,res) => {
     const {id} = req.params
     const {names, lastnames, dni, mail, password, phone_number} = req.body
     try {
-        const [user] = await pool.query("UPDATE users SET names = IFNULL(?, names), lastnames = IFNULL(?, lastnames), dni = IFNULL(?, dni), mail = IFNULL(?, mail), password = IFNULL(?, password), phone_number = IFNULL(?, phone_number) WHERE id_user = ?", [names, lastnames, dni, mail, password, phone_number, id])
+        const [user] = await pool.query("UPDATE Users SET names = IFNULL(?, names), lastnames = IFNULL(?, lastnames), dni = IFNULL(?, dni), mail = IFNULL(?, mail), password = IFNULL(?, password), phone_number = IFNULL(?, phone_number) WHERE id_user = ?", [names, lastnames, dni, mail, password, phone_number, id])
         if (user.affectedRows == 0) return res.status(404).json({
             message : "No se ha actualizado nada"
         })
-        const [users] = await pool.query("SELECT * FROM users WHERE id_user = ?", [id])
+        const [users] = await pool.query("SELECT * FROM Users WHERE id_user = ?", [id])
         res.json(users)
     } catch (error) {
         return res.status(500).json({
@@ -66,10 +66,10 @@ export const putUsers = async(req,res) => {
 export const patchUserStatus = async(req,res) => {
     const {id} = req.params
     try {
-        const [users] = await pool.query("SELECT * FROM users WHERE id_user = ?", [id])
+        const [users] = await pool.query("SELECT * FROM Users WHERE id_user = ?", [id])
         const newStatus = users[0].status == 1 ?  0 : 1;
         await pool.query("UPDATE users SET status = ? WHERE id_user = ?", [newStatus, id])
-        const [user] = await pool.query("SELECT * FROM users WHERE id_user = ?", [id])
+        const [user] = await pool.query("SELECT * FROM Users WHERE id_user = ?", [id])
         res.json(user)
     } catch (error) {
         return res.status(500).json({
@@ -81,7 +81,7 @@ export const patchUserStatus = async(req,res) => {
 export const deleteUsers = async(req,res) => {
     const {id} = req.params
     try {
-        const [user] = await pool.query("DELETE FROM users WHERE id_user = ?", [id])
+        const [user] = await pool.query("DELETE FROM Users WHERE id_user = ?", [id])
         if (user.affectedRows <= 0) return res.status(404).json({
             message: "No se ha eliminado ningún usuario"
         })
