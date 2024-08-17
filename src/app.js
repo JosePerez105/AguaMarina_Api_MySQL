a91830c33ad5ada86636ec33f371af2c9dd4e47b
JosePerez <joseleopepe1033@gmail.com>
2024-08-17 02:24:21 -0500

    Cors Origin 3000

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ import purchasesRoutes from './routes/purchases.routes.js'
 import codesRoutes from './routes/codes.routes.js'
 
 const app = express()
-app.use(cors({origin : '*', credentials: true}))
+app.use(cors({origin : 'http://localhost:3000', credentials: true}))
 
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
