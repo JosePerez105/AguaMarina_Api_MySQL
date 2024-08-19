30029ac64e390138dc271bd21eeea2de7030704f
JosePerez <joseleopepe1033@gmail.com>
2024-08-19 11:33:29 -0500

    Origin cambiado del Cors

--- src/app.js
+++ src/app.js
@@ -18,15 +18,8 @@ import codesRoutes from './routes/codes.routes.js'
 
 const app = express()
 
-const allowedOrigins = ['http://localhost:3000'];
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, origin);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: "http://localhost:3000",
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization, Access-Control-Allow-Origin',
